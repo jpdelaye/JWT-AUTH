f1bc37463d09f0651c4bd9e0c75a5360880c12cc
Jean Paul Delaye <92291163+jpdelaye@users.noreply.github.com>
2023-07-28 16:50:43 -0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,1 +1,22 @@
+// *******************************************************
+// ************ CONNECT TO DATABASE          *************
+// *******************************************************
+var mysql = require('mysql');
+require("dotenv").config();
+
+ 
+const con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user:  process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT 
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  });
+
+  module.exports = con ;
 
